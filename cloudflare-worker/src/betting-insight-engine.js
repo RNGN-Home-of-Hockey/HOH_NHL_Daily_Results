327b564c1c58fe41b524dae93ebc3238eb776b67
znamteam-max <znamteam@gmail.com>
2026-09-11 04:57:14 +0000

    Integrate advanced 5v5 context into betting insights

--- cloudflare-worker/src/betting-insight-engine.js
+++ cloudflare-worker/src/betting-insight-engine.js
@@ -2,6 +2,7 @@ import { withDemoOdds } from "./demo-winline-odds.js";
 import { buildFeatureMarketInsights } from "./feature-market-insights.js";
 import { buildUniversalMarketInsights } from "./universal-market-evaluator.js";
 import { buildRollingLeagueRankInsights } from "./rolling-league-ranks.js";
+import { buildAdvancedMarketContextInsights } from "./advanced-market-context.js";
 
 const EAST = new Set([
   "BOS","BUF","CAR","CBJ","DET","FLA","MTL","NJD","NYI","NYR","OTT","PHI","PIT","TBL","TOR","WSH",
@@ -59,6 +60,7 @@ export async function buildBettingInsights(db, game) {
   const featureInsights = await buildFeatureMarketInsights(db, game);
   const universalMarketInsights = await buildUniversalMarketInsights(db, game);
   const rollingRankInsights = await buildRollingLeagueRankInsights(db, game);
+  const advancedContextInsights = await buildAdvancedMarketContextInsights(db, game);
 
   const insights = [];
   insights.push(...momentumInsights(momentumR.results || [], game));
@@ -69,6 +71,7 @@ export async function buildBettingInsights(db, game) {
   insights.push(...conferenceInsights(conferenceR.results?.[0] || null, game));
   insights.push(...universalMarketInsights);
   insights.push(...rollingRankInsights);
+  insights.push(...advancedContextInsights);
   insights.push(...featureInsights);
 
   return dedupe(insights)
