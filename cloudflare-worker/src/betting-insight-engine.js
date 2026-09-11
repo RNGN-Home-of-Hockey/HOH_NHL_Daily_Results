5e6e9cda13fff1cfed88968aa01f467a12a9781c
znamteam-max <znamteam@gmail.com>
2026-09-11 04:49:38 +0000

    Integrate universal market evaluator into betting insights

--- cloudflare-worker/src/betting-insight-engine.js
+++ cloudflare-worker/src/betting-insight-engine.js
@@ -1,5 +1,6 @@
 import { withDemoOdds } from "./demo-winline-odds.js";
 import { buildFeatureMarketInsights } from "./feature-market-insights.js";
+import { buildUniversalMarketInsights } from "./universal-market-evaluator.js";
 
 const EAST = new Set([
   "BOS","BUF","CAR","CBJ","DET","FLA","MTL","NJD","NYI","NYR","OTT","PHI","PIT","TBL","TOR","WSH",
@@ -55,6 +56,7 @@ export async function buildBettingInsights(db, game) {
   ]);
 
   const featureInsights = await buildFeatureMarketInsights(db, game);
+  const universalMarketInsights = await buildUniversalMarketInsights(db, game);
 
   const insights = [];
   insights.push(...momentumInsights(momentumR.results || [], game));
@@ -63,6 +65,7 @@ export async function buildBettingInsights(db, game) {
   insights.push(...periodInsights(periodR.results || [], game));
   insights.push(...h2hInsights(h2hR.results || [], game));
   insights.push(...conferenceInsights(conferenceR.results?.[0] || null, game));
+  insights.push(...universalMarketInsights);
   insights.push(...featureInsights);
 
   return dedupe(insights)
